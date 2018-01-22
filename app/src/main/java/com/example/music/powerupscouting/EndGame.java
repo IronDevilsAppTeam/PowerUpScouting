dfb2a752c6c62642e770fce4922eedcb0804c5e3
IronDevilsAppTeam <irondevilsappdev@gmail.com>
2018-01-22 17:02:56 -0500

    1/22/18 Carl 2

--- app/src/main/java/com/example/music/powerupscouting/EndGame.java
+++ app/src/main/java/com/example/music/powerupscouting/EndGame.java
@@ -1,7 +1,9 @@
 package com.example.music.powerupscouting;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EndGame extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class EndGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
     }
+
+    public void navigatePostGame (View v){
+        Intent intent = new Intent(this,PostGame.class);
+        startActivity(intent);
+    }
 }
+
