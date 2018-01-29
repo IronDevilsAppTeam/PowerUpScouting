33e9cd009498ba0f27d882620455f54006d0a6a5
IronDevilsAppTeam <irondevilsappdev@gmail.com>
2018-01-29 16:35:03 -0500

    1/29/18 More Save Stuff

--- app/src/main/java/com/example/music/powerupscouting/PostGame.java
+++ app/src/main/java/com/example/music/powerupscouting/PostGame.java
@@ -2,8 +2,14 @@ package com.example.music.powerupscouting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class PostGame extends AppCompatActivity {
+
+    RadioGroup radCardGiven;
+    RadioGroup radMatchResults;
+    EditText etRelevantComments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
